926828dea4707afb87da5c4f978be2082597500f
AboFawaz <azooz@dev.aboFawaz>
2024-06-13 19:45:56 +0300

    Players score The info bar shows palyers score

--- static/main.js
+++ static/main.js
@@ -12,11 +12,20 @@ const Turn = {
 	}
 }
 
+const Score = {
+	player1: 0,
+	player2: 0
+}
+
 const turnBox = document.getElementById('current-turn');
+const player1 = document.getElementById('p1-score');
+const player2 = document.getElementById('p2-score');
 const XOBordElement = document.getElementById('xo-game-board');
 const XOBordElementBoxes = XOBordElement.children;
 
 turnBox.innerText = "X";
+player1.innerText = Score.player1;
+player2.innerText = Score.player2;
 
 for (let i = 0; i < XOBordElementBoxes.length; i++) {
 	const box = XOBordElementBoxes[i]
