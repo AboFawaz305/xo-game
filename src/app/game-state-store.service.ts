a6aa8548e71020191757165c71dc521ab93c4725
AboFawaz305 <you@example.com>
2024-07-17 00:04:20 +0300

    ADDED support for players score

--- src/app/game-state-store.service.ts
+++ src/app/game-state-store.service.ts
@@ -26,6 +26,12 @@ export class GameStateStoreService {
     if (localStorage.getItem('winner') === null) {
       localStorage.setItem('winner', '');
     }
+    if (localStorage.getItem('player1Score') === null) {
+      localStorage.setItem('player1Score', '0');
+    }
+    if (localStorage.getItem('player2Score') === null) {
+      localStorage.setItem('player2Score', '0');
+    }
   }
 
   getBoxState(row: number, col: number): XOSlot {
@@ -49,11 +55,11 @@ export class GameStateStoreService {
     localStorage.setItem('state', JSON.stringify(state))
   }
 
-  getTurn(): XOSlot | null {
+  getTurn(): XOSlot {
     const ls = localStorage.getItem('turn');
     if (ls === null) {
       console.error('game state not initialized')
-      return null;
+      return '';
     }
     //@ts-ignore
     return ls;
@@ -88,6 +94,22 @@ export class GameStateStoreService {
     localStorage.setItem('winner',
       w === 'x' ? 'X wins' : w === 'o' ? 'O wins' : 'draw'
     )
+  }
+
+  getPlayerScore(id: number): number {
+    const score = localStorage.getItem(`player${id}Score`);
+    if (score === null) {
+      return 0;
+    }
+    return +score
+  }
+
+  incrementPlayerScore(id: number) {
+    const score = localStorage.getItem(`player${id}Score`);
+    if (score === null) {
+      return;
+    }
+    localStorage.setItem(`player${id}Score`, (parseInt(score) + 1).toString())
   }
 
   reset() {
